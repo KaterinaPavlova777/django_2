753c70b1b5c4adf77dfee638a76c343b3f07a477
Ekaterina Pavlova <miss.kat.katerina2007@gmail.com>
2024-12-16 09:57:40 +0300

    end

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path
-from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
+from django.contrib import admin
+from django.urls import path, include
 
-app_name = CatalogConfig.name
 
 urlpatterns = [
-     path('', home, name='home'),
-     path('/contacts/', contacts, name='contacts')
+     path('admin/', admin.site.urls),
+     path('', include('catalog.urls', namespace='catalog'))
 ]
